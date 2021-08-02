eed31b2dc96b4bc398c1cbc53911518805447db7
Andrew Reynolds <andrew_reynolds@mac.com>
2021-08-01 21:30:46 -0700

    working with dispatchers and reducers

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,14 @@
 const redux = require('redux');
 
 const counterReducer = (state={counter:0}, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  switch(action.type){
+    case 'increment':
+      return {counter: state.counter + 1};
+    case 'decrement':
+      return {counter: state.counter - 1};
+    default:
+      return state;
+  }
 };
 
 const store = redux.createStore(counterReducer);
@@ -13,3 +18,12 @@ const counterSubscriber = () => {
 };
 
 store.subscribe(counterSubscriber);
+
+
+console.log(store.getState());
+
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'decrement'});
+store.dispatch({type: 'decrement'});
+store.dispatch({type: 'decrement'});
